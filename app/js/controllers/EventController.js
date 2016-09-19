5684b0ccd620b80042b069c1fd0a12c381273725
arnab.roy <arnab2.roy@citi.com>
2016-09-19 17:56:38 -0500

    Using try catch and the promise object.
    Using try catch and the promise object.

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,8 +3,13 @@ eventsApp.controller('EventController',
     function EventController($scope,$log, eventData) {
 
     $scope.sortCreator='Creator';
-        $scope.event=eventData.getEventData();
 
+        /*$scope.event=eventData.getEventData(); */
+        eventData.getEventData()
+            .$promise
+            .then(function(event){$scope.event=event;console.log(event)})
+            .catch(function (response){console.log(response);}
+        );
 
         //$scope.event=eventData.event;
     $scope.upVoteSession = function upvote(session){
